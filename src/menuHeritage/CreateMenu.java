22218860e9a5929ed32732fa48c189028876f924
vanessahuhn <vanessa.huhn@gmail.com>
2018-07-30 14:15:55 +0200

    ajout des valeurs

--- src/menuHeritage/CreateMenu.java
+++ src/menuHeritage/CreateMenu.java
@@ -16,6 +16,27 @@ public class CreateMenu {
     	Matin menuMatin = new Matin();
     	MidiSoir menuMidi = new MidiSoir();
     	MidiSoir menuSoir = new MidiSoir();
+    	   	
+    	menuMatin.setBoisson("café");
+    	menuMatin.setCereale("muesli");
+    	menuMatin.setFeculent("pain complet");
+    	menuMatin.setFruit("orange");
+    	menuMatin.setProduitLaitier("lait");
+    	
+    	menuMidi.setBoisson("eau");
+    	menuMidi.setFeculent("pâtes");
+    	menuMidi.setFruit("banane");
+    	menuMidi.setLegume("poivron");
+    	menuMidi.setProduitLaitier("yaourt");
+    	menuMidi.setProteine("tofu");
+    	
+    	menuSoir.setBoisson("eau");
+    	menuSoir.setFeculent("riz");
+    	menuSoir.setFruit("pomme");
+    	menuSoir.setLegume("courgette");
+    	menuSoir.setProduitLaitier("fromage");
+    	menuSoir.setProteine("thon");
+    	
     	menuMatin.afficheMenuMatin();
     	menuMidi.afficheMenuMidiSoir();
     	menuSoir.afficheMenuMidiSoir();
